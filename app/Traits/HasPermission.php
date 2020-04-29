695d658c472e540d9807b2b14ebd65a40cdd30b9
she-writescode <busolaokemoney@gmail.com>
2020-04-29 21:43:03 +0100

    generate dummydata

--- app/Traits/HasPermission.php
+++ app/Traits/HasPermission.php
@@ -54,7 +54,7 @@ trait HasPermission
         return $this->belongsToMany(Permission::class);
     }
 
-    public function role()
+    public function roles()
     {
         return $this->belongsToMany(Role::class);
     }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Permission;
+use App\Role;
+use App\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +16,49 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+
+        $models = ['product', 'role', 'user', 'order', 'permission', 'category'];
+        $bread = ['browse', 'read', 'edit', 'add', 'delete'];
+
+        $permissions = [];
+
+        foreach ($models as $model) {
+            foreach ($bread as $action) {
+                $permissions[$model][] = Permission::create([
+                    'slug' => $model . '.' . $action,
+                    'name' => Str::ucfirst($action) . ' ' . Str::ucfirst($model),
+                ]);
+            }
+        }
+
+        $allPermissions = Permission::all();
+
+        $admin = Role::create([
+            'name' => 'Administrator',
+            'slug' => 'admin',
+        ]);
+
+        $admin->permissions()->attach($allPermissions);
+
+        $salesperson = Role::create([
+            'name' => 'Sales Person',
+            'slug' => 'sales-person',
+        ]);
+        $salespersonPermission = Permission::where('slug', 'order.browse')->orWhere('slug', 'order.read')->orWhere('slug', 'order.add')->orWhere('slug', 'product.browse')->orWhere('slug', 'product.read')->orWhere('slug', 'category.browse')->orWhere('slug', 'category.read')->get();
+        $salesperson->permissions()->attach($salespersonPermission);
+
+        $user = User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password')
+        ]);
+        $user->roles()->attach($admin);
+
+        $user2 = User::create([
+            'name' => 'Sales Person User',
+            'email' => 'salesperson@example.com',
+            'password' => bcrypt('password')
+        ]);
+        $user2->roles()->attach($salesperson);
     }
 }
